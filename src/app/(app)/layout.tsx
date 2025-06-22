fe9fb82f3f4161c621950f38705c5d5c04ab4192
erenst <erenst2002@hotmail.com>
2025-06-22 16:28:57 +0930

    12.filtering products

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
 import { TRPCReactProvider } from "@/trpc/client";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+
 import "./globals.css";
+import { DM_Sans } from "next/font/google";
+
 import { Toaster } from "@/components/ui/sonner";
 
 const dmSans = DM_Sans({
@@ -21,10 +24,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${dmSans.className}  antialiased`}>
-        <TRPCReactProvider>
-          {children}
-          <Toaster />
-        </TRPCReactProvider>
+        <NuqsAdapter>
+          <TRPCReactProvider>
+            {children}
+            <Toaster />
+          </TRPCReactProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
